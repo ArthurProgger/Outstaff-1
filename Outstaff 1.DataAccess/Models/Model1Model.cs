a3163303e8eb4cc9be1da76a6d01ac842476cf2b
ArthurProgger <sanyamalkov228@gmail.com>
2025-04-03 16:47:04 +0500

    added comments.

--- Outstaff 1.DataAccess/Models/Model1Model.cs
+++ Outstaff 1.DataAccess/Models/Model1Model.cs
@@ -4,7 +4,18 @@ namespace Outstaff_1.DataAccess.Models;
 
 public sealed class Model1Model : IEntityId<int>
 {
+    /// <summary>
+    /// Идентификатор.
+    /// </summary>
     public int Id { get; set; }
+
+    /// <summary>
+    /// Код.
+    /// </summary>
     public int Code { get; set; }
+
+    /// <summary>
+    /// Значение.
+    /// </summary>
     public string Value { get; set; } = null!;
 }
